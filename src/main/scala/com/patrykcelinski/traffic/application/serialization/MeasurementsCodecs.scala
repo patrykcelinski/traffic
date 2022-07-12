74b727f4ec4ad2dba30ab31f3fa15d816dcdbc1b
Patryk Celiński <patryk.celinski90@gmail.com>
2022-07-12 14:59:57 +0200

    Add measurements codecs

---
+++ src/main/scala/com/patrykcelinski/traffic/application/serialization/MeasurementsCodecs.scala
@@ -0,0 +1,26 @@
+package com.patrykcelinski.traffic.application.serialization
+import cats.implicits._
+import com.patrykcelinski.traffic.domain.model._
+import io.circe.Decoder
+import io.circe.generic.semiauto._
+
+object MeasurementsCodecs {
+
+  implicit val avenueDecoder: Decoder[Avenue] = Decoder.decodeChar.map(
+    Avenue.apply
+  )
+  implicit val streetDecoder: Decoder[Street] =
+    Decoder.decodeString.emap(stringInt =>
+      Either.fromOption(
+        Street.fromString(stringInt),
+        s"Invalid Street format: $stringInt"
+      )
+    )
+
+  implicit val transitMeasurementDecoder: Decoder[TransitMeasurement] =
+    deriveDecoder
+  implicit val trafficMeasurementDecoder: Decoder[TrafficMeasurement] =
+    deriveDecoder
+  implicit val measurementsDecoder: Decoder[Measurements]             = deriveDecoder
+
+}
